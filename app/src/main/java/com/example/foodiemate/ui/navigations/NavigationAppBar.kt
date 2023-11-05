0044a7e996c3a78b2d1b19f46a7e3e683690e6b3
Slava <kruchkovenko@gmail.com>
2023-11-06 00:12:56 +0500

    add search bar items

--- app/src/main/java/com/example/foodiemate/ui/navigations/NavigationAppBar.kt
+++ app/src/main/java/com/example/foodiemate/ui/navigations/NavigationAppBar.kt
@@ -1,18 +1,28 @@
 package com.example.foodiemate.ui.navigations
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.foodiemate.ui.theme.Blue200
+import com.example.foodiemate.ui.theme.Blue400
 import com.example.foodiemate.ui.theme.Blue700
-import com.example.foodiemate.ui.theme.White
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NavigationAppBar() {
+fun NavigationAppBar(searches: List<String>, placeHolder: String) {
     var isActive by remember {
         mutableStateOf(false)
     }
@@ -20,13 +30,13 @@ fun NavigationAppBar() {
         mutableStateOf("")
     }
     SearchBar(
-        modifier = Modifier
-            .fillMaxWidth().padding(0.dp),
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(0.dp),
         colors = SearchBarDefaults.colors(
-            White,
-            Blue700,
+            if (isActive) Blue200 else Color.Transparent,
+            Color.Transparent,
             TextFieldDefaults.colors(
-
+                focusedTextColor = Blue700
             )
         ),
         query = queryString,
@@ -41,12 +51,64 @@ fun NavigationAppBar() {
             isActive = it
         },
         placeholder = {
-            Text(text = "Search")
+            if (!isActive) Text(
+                text = placeHolder,
+                color = Blue700,
+                fontSize = 28.sp,
+                style = TextStyle(lineHeight = 28.sp),
+                overflow = TextOverflow.Visible,
+                modifier = Modifier
+                    .height(28.dp)
+            ) else {
+                Text(text = placeHolder)
+            }
         },
+        leadingIcon = null,
         trailingIcon = {
-            Icon(imageVector = Icons.Default.Search, contentDescription = null)
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(24.dp)
+                    .offset(0.dp, 4.dp),
+                tint = Blue700
+            )
         }
     ) {
+        searches.forEach {
+            var isSelected by remember {
+                mutableStateOf(false)
+            }
+            val defaultModifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp)
+                .clickable {
+                    isSelected = true
+                }
+            val selectModifier = Modifier
+                .clip(RoundedCornerShape(8.dp))
+                .background(Blue400)
+            Column(
+                modifier = defaultModifier
+                    .composed {
+                        if (isSelected) selectModifier else Modifier
+                    }
 
+            ) {
+                Text(
+                    text = it,
+                    modifier = Modifier
+                        .padding(vertical = 12.dp, horizontal = 8.dp)
+                        .fillMaxWidth(),
+                    color = Blue700
+                )
+                if (!isSelected) HorizontalDivider(
+                    color = Blue400,
+                    modifier = Modifier
+                        .height(1.dp)
+                        .fillMaxWidth()
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/example/foodiemate/ui/screens/FridgeScreen.kt
+++ app/src/main/java/com/example/foodiemate/ui/screens/FridgeScreen.kt
@@ -1,6 +1,5 @@
 package com.example.foodiemate.ui.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -9,9 +8,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.foodiemate.R
 import com.example.foodiemate.data.FridgeProduct
 import com.example.foodiemate.data.IndexObject
 import com.example.foodiemate.network.Mock
@@ -28,14 +28,16 @@ fun FridgeScreen() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(vertical = 24.dp, horizontal = 16.dp)
         ) {
-            NavigationAppBar()
+            NavigationAppBar(
+                products.map { it.product.name },
+                stringResource(id = R.string.products)
+            )
             LazyVerticalGrid(
                 columns = GridCells.Fixed(2),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 12.dp, bottom = 64.dp, start = 0.dp, end = 0.dp)
+                    .padding(top = 12.dp, bottom = 80.dp, start = 16.dp, end = 16.dp)
             ) {
                 items(products.mapIndexed { i: Int, product: FridgeProduct ->
                     IndexObject(i, product)
