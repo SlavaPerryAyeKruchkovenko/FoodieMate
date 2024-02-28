6db543da71359b4d0f6ffe7e2bd3d96dd06572a5
Slava <kruchkovenko@gmail.com>
2024-02-28 23:45:46 +0500

    .

--- app/src/main/java/com/example/foodiemate/MainActivity.kt
+++ app/src/main/java/com/example/foodiemate/MainActivity.kt
@@ -5,9 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.foodiemate.ui.navigation.NavigationBottomBar
@@ -18,25 +20,27 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FoodieMateTheme {
+
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = CustomTheme.colors.primaryBackground
+                    modifier = Modifier.fillMaxSize(), color = CustomTheme.colors.primaryBackground
                 ) {
                     val navController = rememberNavController()
+                    val scaffoldState = rememberBottomSheetScaffoldState()
                     Scaffold(
                         modifier = Modifier.fillMaxSize(),
                         floatingActionButtonPosition = FabPosition.Center,
                         bottomBar = {
                             NavigationBottomBar(navController)
                         },
-                    ) {
+                    ) { paddingValues ->
                         NavigationGraph(
                             navController = navController,
-                            modifier = Modifier.padding(bottom = it.calculateBottomPadding())
+                            modifier = Modifier.padding(bottom = paddingValues.calculateBottomPadding())
                         )
                     }
                 }
