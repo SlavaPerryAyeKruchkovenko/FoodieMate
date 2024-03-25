fcc62ec3c5c164ca6d88210eefa54eed1ef088cc
Slava <kruchkovenko@gmail.com>
2024-03-26 01:08:00 +0500

    add padding for last product

--- app/src/main/java/com/example/foodiemate/ui/screens/fridge/component/FridgeProductList.kt
+++ app/src/main/java/com/example/foodiemate/ui/screens/fridge/component/FridgeProductList.kt
@@ -23,36 +23,40 @@ fun FridgeProductList(
     editableProduct: MutableState<FridgeProduct?>,
     editProductCount: (product: FridgeProduct, value: Number, unit: UnitOfMeasure) -> Unit
 ) {
+    val lastProductPadding =
+        CustomTheme.layoutSize.addFABSize + CustomTheme.layoutPadding.addFABPadding * 2
     LazyVerticalGrid(
         columns = GridCells.Fixed(1), modifier = Modifier
             .fillMaxSize()
             .padding(
                 top = 12.dp,
                 start = CustomTheme.layoutPadding.smallPadding,
-                end = CustomTheme.layoutPadding.smallPadding
+                end = CustomTheme.layoutPadding.smallPadding,
             )
     ) {
         items(items.value.mapIndexed { i: Int, product: FridgeProduct ->
             IndexObject(i, product)
-        }) { (_, product) ->
+        }) { (index, product) ->
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(top = 8.dp)
+                    .then(
+                        if (index == items.value.size - 1) {
+                            Modifier.padding(top = 8.dp, bottom = lastProductPadding)
+                        } else {
+                            Modifier.padding(top = 8.dp)
+                        }
+                    )
             ) {
                 val isEdit = editableProduct.value != null && editableProduct.value == product
-                FridgeProductView(product = product,
-                    isEdit = isEdit,
-                    onEnableEditProduct = {
-                        editableProduct.value = product
-                    },
-                    onEditProduct = { value, unit ->
-                        editProductCount(product, value, unit)
-                    },
-                    onDisableEditProduct = {
-                        editableProduct.value = null
-                    })
+                FridgeProductView(product = product, isEdit = isEdit, onEnableEditProduct = {
+                    editableProduct.value = product
+                }, onEditProduct = { value, unit ->
+                    editProductCount(product, value, unit)
+                }, onDisableEditProduct = {
+                    editableProduct.value = null
+                })
             }
         }
     }
